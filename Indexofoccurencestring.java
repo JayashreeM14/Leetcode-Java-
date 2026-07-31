4e8c860d5cf13bfc215ba9a90ab6af9a9cb15301
JayashreeM14 <jayashree.m1411@gmail.com>
2026-07-31 22:01:02 +0530

    Leetcode problems

---
+++ Indexofoccurencestring.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+       {
+         if ( haystack.equals(needle))
+         
+            System.out.println("0");
+            else
+            System.out.println("-1");
+       }
+       return haystack.indexOf(needle);
+    }
+
+}
